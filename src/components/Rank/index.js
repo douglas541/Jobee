d186a5c080a77fc340bce541d1b750d17961002f
Douglas Sousa <douglas@pop-os.localdomain>
2021-01-17 17:47:26 -0300

    feat: Rank component minor changes

--- src/components/Rank/index.js
+++ src/components/Rank/index.js
@@ -3,12 +3,14 @@ import React, { Profiler } from 'react';
 import './styles.css'
 
 function Rank(props) {
-   const { content } = props;
+   const { position, area, content, topics } = props;
 
    return (
-         <div id="container-rank">
-            <p>• { content }</p>
-         </div>
+      <div id="container-rank">
+         <p1>{position}. {area}</p1><br/>
+         <p2>{content}</p2><br/>
+         <p3>Tópicos sobre o assunto: {topics}</p3>
+      </div>
    )
 }
 
--- src/pages/MainRank/index.js
+++ src/pages/MainRank/index.js
@@ -6,48 +6,151 @@ import Rank from '../../components/Rank'
 import './styles.css'
 
 function MainRank() {
-   
-   const[corSubNav1, setCorSubNav1] = useState("600");
-   const[corSubNav2, setCorSubNav2] = useState("500");
-   const[corSubNav3, setCorSubNav3] = useState("500");
+
+   const [corSubNav1, setCorSubNav1] = useState("600");
+   const [corSubNav2, setCorSubNav2] = useState("500");
+   const [corSubNav3, setCorSubNav3] = useState("500");
 
    const [rankComps, setRankComps] = useState([
-      { content: "Desenvolvedor Web" },
-      { content: "Engenheiro de Software" },
-      { content: "UX Designer" },
-      { content: "músico" },
-      { content: "Publicitário" },
-      { content: "Arquiteto" },
-      { content: "Professor" },
-      { content: "Dentista" },
-      { content: "Advogado" },
-      { content: "UI Designer" },
+      {
+         position: "1",
+         area: "TI",
+         topics: "131",
+         content: "Desenvolvedor Web"
+      },
+      {
+         position: "2",
+         area: "Design",
+         topics: "126",
+         content: "UX Designer"
+      },
+      {
+         position: "3",
+         area: "Poublicidade e Propaganda",
+         topics: "117",
+         content: "Publicitário"
+      },
+      {
+         position: "4",
+         area: "Poublicidade e Propaganda",
+         topics: "109",
+         content: "Marketing Digital"
+      },
+      {
+         position: "5",
+         area: "Arquitetura e Urbanismo",
+         topics: "102",
+         content: "Projetista"
+      },
+      {
+         position: "6",
+         area: "Educação",
+         topics: "96",
+         content: "Professor Particular"
+      },
+      {
+         position: "7",
+         area: "Eletrônica",
+         topics: "91",
+         content: "Estagiário de Eletrônica"
+      },
+      {
+         position: "8",
+         area: "TI",
+         content: "Engenheiro de Software",
+         topics: "83"
+      },
+      {
+         position: "9",
+         area: "Música",
+         topics: "76",
+         content: "Professor de Música"
+      },
+      {
+         position: "10",
+         area: "Design",
+         topics: "73",
+         content: "UI Designer"
+      },
    ]);
 
    const rankList = rankComps.map((rank) => {
       return (
          <Rank
-            key={rank.content}
+            key={rank.position}
+            position={rank.position}
+            area={rank.area}
             content={rank.content}
+            topics={rank.topics}
          />
       );
    });
-   
+
 
    const listProfessions = () => {
       setRankComps([
-         { content: "Desenvolvedor Web" },
-         { content: "Engenheiro de Software" },
-         { content: "UX Designer" },
-         { content: "Músico" },
-         { content: "Publicitário" },
-         { content: "Arquiteto" },
-         { content: "Professor" },
-         { content: "Dentista" },
-         { content: "Advogado" },
-         { content: "UI Designer" },
+         {
+            position: "1",
+            area: "TI",
+            topics: "131",
+            content: "Desenvolvedor Web"
+         },
+         {
+            position: "2",
+            area: "Design",
+            topics: "126",
+            content: "UX Designer"
+         },
+         {
+            position: "3",
+            area: "Poublicidade e Propaganda",
+            topics: "117",
+            content: "Publicitário"
+         },
+         {
+            position: "4",
+            area: "Poublicidade e Propaganda",
+            topics: "109",
+            content: "Marketing Digital"
+         },
+         {
+            position: "5",
+            area: "Arquitetura e Urbanismo",
+            topics: "102",
+            content: "Projetista"
+         },
+         {
+            position: "6",
+            area: "Educação",
+            topics: "96",
+            content: "Professor Particular"
+         },
+         {
+            position: "7",
+            area: "Eletrônica",
+            topics: "91",
+            content: "Estagiário de Eletrônica"
+         },
+         {
+            position: "8",
+            area: "TI",
+            content: "Engenheiro de Software",
+            topics: "83"
+         },
+         {
+            position: "9",
+            area: "Música",
+            topics: "76",
+            content: "Professor de Música"
+         },
+         {
+            position: "10",
+            area: "Design",
+            topics: "73",
+            content: "UI Designer"
+         },
       ]);
-      
+
       setCorSubNav1("600");
       setCorSubNav2("500");
       setCorSubNav3("500");
@@ -56,16 +159,66 @@ function MainRank() {
 
    const listHardSkills = () => {
       setRankComps([
-         { content: "Gestão de projetos" },
-         { content: "Mecânica de motores" },
-         { content: "Programação" },
-         { content: "Domínio de PHP" },
-         { content: "Domínio de excel avançado" },
-         { content: "Contabilidade" },
-         { content: "Gestão de pessoas" },
-         { content: "Inglês" },
-         { content: "Operação de máquinas" },
-         { content: "Espanhol" },
+         {
+            position: "1",
+            area: "TI",
+            topics: "131",
+            content: "Gestão de projetos"
+         },
+         {
+            position: "2",
+            area: "Mecânica",
+            topics: "126",
+            content: "Mecânica de motores"
+         },
+         {
+            position: "3",
+            area: "TI",
+            topics: "117",
+            content: "Programação"
+         },
+         {
+            position: "4",
+            area: "TI",
+            topics: "109",
+            content: "Domínio de PHP"
+         },
+         {
+            position: "5",
+            area: "Tecnologia em Informática",
+            topics: "102",
+            content: "Domínio de excel avançado"
+         },
+         {
+            position: "6",
+            area: "Contabilidade",
+            topics: "96",
+            content: "Contabilidade"
+         },
+         {
+            position: "7",
+            area: "Administração",
+            topics: "91",
+            content: "Gestão de pessoas"
+         },
+         {
+            position: "8",
+            area: "Linguagens",
+            content: "Inglês",
+            topics: "83"
+         },
+         {
+            position: "9",
+            area: "Mecânica",
+            topics: "76",
+            content: "Operação de máquinas"
+         },
+         {
+            position: "10",
+            area: "Linguagens",
+            topics: "73",
+            content: "Espanhol"
+         },
       ]);
 
       setCorSubNav1("500");
@@ -75,25 +228,69 @@ function MainRank() {
 
    const listSoftSkills = () => {
       setRankComps([
-         { content: "Negociação e Persuasão" },
-         { content: "Inteligência Criativa" },
-         { content: "Gestão de Pessoas" },
-         { content: "Flexibilidade"},
-         { content: "Pensamento Critico"},
-         { content: "Liderança"},
-         { content: "Resiliência" },
-         { content: "Proatividade" },
-         { content: "Comunicação" },
-         { content: "Persuação" },
+         {
+            position: "1",
+            area: "Soft Skill",
+            topics: "131",
+            content: "Negociação e Persuasão"
+         },
+         {
+            position: "2",
+            area: "Soft Skill",
+            topics: "126",
+            content: "Inteligência Criativa"
+         },
+         {
+            position: "3",
+            area: "Soft Skill",
+            topics: "117",
+            content: "Gestão de Pessoas"
+         },
+         {
+            position: "4",
+            area: "Soft Skill",
+            topics: "109",
+            content: "Flexibilidade"
+         },
+         {
+            position: "5",
+            area: "Soft Skill",
+            topics: "102",
+            content: "Pensamento Critico"
+         },
+         {
+            position: "6",
+            area: "Soft Skill",
+            topics: "96",
+            content: "Resiliência"
+         },
+         {
+            position: "7",
+            area: "Soft Skill",
+            topics: "91",
+            content: "Proatividade"
+         },
+         {
+            position: "8",
+            area: "Soft Skill",
+            content: "Comunicação",
+            topics: "85"
+         },
+         {
+            position: "9",
+            area: "Soft Skill",
+            topics: "76",
+            content: "Persuação"
+         }
       ]);
 
-      
+
       setCorSubNav1("500");
       setCorSubNav2("500");
       setCorSubNav3("600");
 
    }
-   
+
    return (
       <div>
          <div id="main-rank-page">
@@ -114,9 +311,9 @@ function MainRank() {
 
                <div id="main-rank-page-subnav-container">
                   <nav id="main-rank-page-subnav">
-                     <a href="#" style={{fontWeight: corSubNav1}} id="profissoes" onClick= {listProfessions}>Profissões</a>
-                     <a href="#" style={{fontWeight: corSubNav2}} id="hardskills" onClick= {listHardSkills}>Hard Skills</a>
-                     <a href="#" style={{fontWeight: corSubNav3}} id="listskills" onClick= {listSoftSkills}>Soft Skills</a>
+                     <a href="#" style={{ fontWeight: corSubNav1 }} id="profissoes" onClick={listProfessions}>Profissões</a>
+                     <a href="#" style={{ fontWeight: corSubNav2 }} id="hardskills" onClick={listHardSkills}>Hard Skills</a>
+                     <a href="#" style={{ fontWeight: corSubNav3 }} id="listskills" onClick={listSoftSkills}>Soft Skills</a>
                   </nav>
                </div>
                <div id="main-rank-post-container">
